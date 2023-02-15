8864152e16e9a9c0ea462c7008ce7c4a75564ecc
Anxo Campos <anxo.campos260204@gmail.com>
2023-02-15 11:28:59 +0100

    all

--- 2022-2023/Anxito/mysql/sakila/practicaSakila2.sql
+++ 2022-2023/Anxito/mysql/sakila/practicaSakila2.sql
@@ -84,36 +84,51 @@ ORDER BY 1 DESC
 LIMIT 1)
 ORDER BY 2 DESC;
 
-#10-11
-SELECT AVG(peliculas.duracion) AS "Duracion media alquiladas"
+
+#10 - 11
+SELECT AVG(f.length) AS "duracion"
+FROM rental r JOIN inventory i USING(inventory_id) JOIN film f USING(film_id)
+WHERE return_date IS NULL;
+
+SELECT AVG(peliculas.duracion) AS "Duración media alquiladas"
 FROM
-(SELECT DISTINCT(f.film_id), f.length AS "duracion"
+(SELECT DISTINCT(f.film_id),f.length AS "duracion"
 FROM rental r JOIN inventory i USING(inventory_id) JOIN film f USING(film_id)
 WHERE return_date IS NULL) peliculas;
 
 #12
-SELECT fc.category_id, COUNT(*) AS "Alquileres infantil"
+
+SELECT i.store_id,COUNT(*) AS "Alquileres infantil"
 FROM rental r JOIN inventory i USING(inventory_id) JOIN film_category fc USING(film_id) JOIN category ca USING(category_id)
 WHERE ca.name="CHILDREN" AND YEAR(r.rental_date)=2005 AND MONTH(r.rental_date) IN (8,9)
 GROUP BY i.store_id
 ORDER BY 2 DESC;
 
-SELECT fc.category_id, COUNT(*) AS "Alquileres infantil"
+SELECT i.store_id,COUNT(*) AS "Alquileres infantil"
 FROM rental r JOIN inventory i USING(inventory_id) JOIN film_category fc USING(film_id) JOIN category ca USING(category_id)
 WHERE ca.name="CHILDREN" AND r.rental_date BETWEEN '2005-08-01' AND '2005-09-30'
 GROUP BY i.store_id
 ORDER BY 2 DESC;
 
-SELECT i.store_id, COUNT(*)
-FROM inventory i
-GROUP BY i.store_id;
-
 #13
 
-SELECT s.staff_id, c.city,st.store_id
-FROM staff s JOIN address a USING(address_id) JOIN city cc USING(city_id) JOIN store st USING(store_id) JOIN address ad ON st.address_id=ad.address
+SELECT s.staff_id,c.city,st.store_id,cit.city
+FROM staff s JOIN address a USING(address_id) JOIN city c USING(city_id) JOIN store st USING(store_id) JOIN address ad ON st.address_id=ad.address_id JOIN city cit ON ad.city_id=cit.city_id
+WHERE c.city_id=cit.city_id;
 
+#14
+SELECT customer_id,first_name,last_name,SUM(amount)
+FROM payment JOIN customer USING(customer_id)
+GROUP BY customer_id
+ORDER BY 4 DESC
+LIMIT 10;
 
+#15
+
+SELECT f.title,SUM(p.amount)
+FROM film f JOIN inventory i USING(film_id) JOIN rental r USING(inventory_id) JOIN payment p USING(rental_id)
+GROUP BY f.film_id
+ORDER BY 2 DESC LIMIT 10;
 
 
 
---
+++ 2022-2023/Anxito/mysql/sakila/practicaSakila3.sql
@@ -0,0 +1,5 @@
+#24 Calcula ahora las 10 películas que más dinero han generado a la empresa.
+SELECT *
+FROM Customer
+
+
