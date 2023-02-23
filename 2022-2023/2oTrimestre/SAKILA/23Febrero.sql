c87bb7996330d7123abdc26d8eb5348783eb685b
gaalpos <gabripazos13@gmail.com>
2023-02-23 11:52:24 +0100

    02/23/2023 11:52:24

---
+++ 2022-2023/2oTrimestre/SAKILA/23Febrero.sql
@@ -0,0 +1,6 @@
+## Obtén los actores que han trabajado en todas las películas en las que ha trabajado Christopher West
+select fa.film_id
+from actor a join film_actor fa using (actor_id)
+where first_name="Christopher" and last_name="WEST";
+
+## Clientes con más películas alquiladas que alguno de los clientes que empiezan por ‘A’
