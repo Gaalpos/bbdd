a61e134e96f8471f06c0fc8f19796c2a439dbdc1
gaalpos <gabripazos13@gmail.com>
2023-04-13 10:00:20 +0200

    04/13/2023 10:00:19

---
+++ 2022-2023/3erTrimestre/src/Principal.java
@@ -0,0 +1,34 @@
+import java.sql.Connection;
+import java.sql.Statement;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		}catch(ClassNotFoundException e){
+			System.out.println(" no ha funcionado");
+		}
+		
+		Connection conexion;
+		try {
+			String url= "jdbc:mysql://localhost:3306/sakila";
+			 conexion = DriverManager.getConnection(url,"root", "1234");
+			 
+			 System.out.println("Conexion establecida");
+			 /*
+			 Statement s = con.createStatement();
+			 s.execute("USE sakila");
+			 
+			 ResultSet result=s.executeQuery("select * from film");
+			 */
+			
+		} catch (SQLException e) {
+			System.out.println(" no ha funcionado");
+		}
+	}
+
+}
